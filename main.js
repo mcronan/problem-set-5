a4ed360798823a6df164987a8a386bfce170859e
Ronan <ronanir600@gmail.com>
2015-08-03 09:06:11 -0600

    problem 1 finished

---
+++ main.js
@@ -0,0 +1,11 @@
+var alphabetSoup = function(word){
+
+	var myArray = word.split("")
+		var ordered = myArray.sort()
+		var stringer = ordered.join('')
+		console.log(stringer)
+	}
+	
+
+alphabetSoup('hello')
+
